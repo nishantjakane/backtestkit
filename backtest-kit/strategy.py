6feb0e2d7a360d11aeeaaf2b1317edd2937fbbeb
Nishant212121 <savitarjakane@gmail.com>
2026-08-10 23:37:22 +0530

    feat: add strategy class and createOrder

---
+++ backtest-kit/strategy.py
@@ -0,0 +1,25 @@
+from order import Order,OrderStatus,OrderType,Side
+
+class Strategy():
+    def __init__(self):
+        self.order_count =0
+        self.history = []
+        self.current_candle = None
+
+    def on_bar(self,candle):
+        return None
+
+    def generate_order_id(self):
+        self.order_count+=1
+        return self.order_count
+
+    def createOrder(self,side,order_type,price,qty) -> Order:
+        return Order(
+            self.generate_order_id(),
+            side,
+            order_type,
+            price,
+            qty,
+            OrderStatus.PENDING,
+            timestamp
+        )
