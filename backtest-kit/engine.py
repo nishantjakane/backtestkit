d4e9f94e3a6e4f2643792763002de30adeeb476e
Nishant212121 <savitarjakane@gmail.com>
2026-08-11 22:57:48 +0530

    feat: add process orders

--- backtest-kit/engine.py
+++ backtest-kit/engine.py
@@ -1,11 +1,13 @@
 from data import load_data,Candle,row_to_candle
+from order import Side , OrderStatus, OrderType
 
 class Engine:
     def __init__(self,file_path,strategy):
         self.file_path = file_path
-        self.strategy = strategy()
+        self.strategy = strategy(self)
 
         self.data = load_data(file_path)
+        self.orders = []
 
 
     def run(self):
@@ -17,3 +19,22 @@ class Engine:
             self.strategy.current_candle = candle
             self.strategy.on_bar(candle)
             self.strategy.history.append(candle)
+    
+    def submit_order(self,order):
+        self.orders.append(order)
+
+    def process_orders(self,candle):
+        open = candle.open
+        high = candle.high
+        low = candle.low
+        close = candle.close
+        datetime = candle.datetime
+
+        for order in self.orders:
+            if order.order_type == OrderType.LIMIT:
+                if order.side == Side.BUY:
+                    if high >= order.price and low <=order.price:
+                        order.status = OrderStatus.FILLED
+                if order.side == Side.SELL:
+                    if high >= order.price and low <=order.price:
+                        order.status = OrderStatus.FILLED
