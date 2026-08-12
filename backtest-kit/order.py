546d8143effb412c6bf7a1b6a5a3d69acee86c8c
Nishant212121 <savitarjakane@gmail.com>
2026-08-12 23:45:05 +0530

    feat:add execute and cancelorder

--- backtest-kit/order.py
+++ backtest-kit/order.py
@@ -14,12 +14,23 @@ class OrderStatus(Enum):
     CANCELLED = "CANCELLED"
 
 class Order():
-    def __init__(self,order_id,side,order_type,price,qty,status,timestamp):
+    self.executed_timestamp = None 
+
+    def __init__(self,order_id,side,order_type,price,qty,take_profit,stop_loss,timestamp):
         self.order_id = order_id
         self.side = side
         self.order_type= order_type 
         self.price = price
-        self.qty = qty
-        self.status = status
+        self.qty = qty  
+        self.take_profit=take_profit
+        self.stop_loss=stop_loss
+        self.status = OrderStatus.PENDING
         self.timestamp = timestamp
+
+    def execute(self,candle):
+        self.status = OrderStatus.FILLED
+        self.executed_timestamp = candle.datetime
+
+    def cancelOrder(self):
+        self.status=OrderStatus.CANCELLED
 
--- backtest-kit/strategy.py
+++ backtest-kit/strategy.py
@@ -1,28 +1,33 @@
 from order import Order,OrderStatus,OrderType,Side
 
 class Strategy():
-    def __init__(self):
+    def __init__(self,engine):
         self.order_count =0
         self.history = []
         self.current_candle = None
+        self.engine = engine
 
     def init(self):
         pass
 
     def on_bar(self,candle):
-        return None
+        pass
 
     def generate_order_id(self):
         self.order_count+=1
         return self.order_count
 
     def createOrder(self,side,order_type,price,qty) -> Order:
-        return Order(
+        order = Order(
             self.generate_order_id(),
             side,
             order_type,
             price,
             qty,
             OrderStatus.PENDING,
-            timestamp
+            self.current_candle.datetime
         )
+
+        self.engine.submit_order(order)
+        
+        return order
