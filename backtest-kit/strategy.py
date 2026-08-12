ddec79e12ffa2c6dc1396d5d567f28eb244fe4d6
Nishant212121 <savitarjakane@gmail.com>
2026-08-12 23:45:45 +0530

    feat:add trade and position id gen

--- backtest-kit/strategy.py
+++ backtest-kit/strategy.py
@@ -3,6 +3,8 @@ from order import Order,OrderStatus,OrderType,Side
 class Strategy():
     def __init__(self,engine):
         self.order_count =0
+        self.position_count =0
+        self.trade_count=0
         self.history = []
         self.current_candle = None
         self.engine = engine
@@ -17,6 +19,14 @@ class Strategy():
         self.order_count+=1
         return self.order_count
 
+    def generate_position_id(self):
+        self.position_count+=1
+        return self.position_count
+
+    def generate_trade_id(self):
+        self.trade_count+=1
+        return self.trade_count
+
     def createOrder(self,side,order_type,price,qty) -> Order:
         order = Order(
             self.generate_order_id(),
@@ -24,7 +34,6 @@ class Strategy():
             order_type,
             price,
             qty,
-            OrderStatus.PENDING,
             self.current_candle.datetime
         )
 
